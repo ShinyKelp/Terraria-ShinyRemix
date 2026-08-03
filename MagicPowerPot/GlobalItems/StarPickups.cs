f4cab279144a5dc872830641461fd456571003aa
ShinyKelp <theobusiness11@gmail.com>
2026-08-03 17:27:45 +0200

    added magic power tweak

---
+++ MagicPowerPot/GlobalItems/StarPickups.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShinyRemix.MagicPowerPot.GlobalItems
+{
+    public class StarPickups : GlobalItem
+    {
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+        {
+            return ShinyOptions.MagicPowerBoost && (entity.type == ItemID.Star || entity.type == ItemID.SoulCake || entity.type == ItemID.SugarPlum);
+        }
+
+        public override bool OnPickup(Item item, Player player)
+        {
+            if(!ShinyUtils.TRAE && player.HasBuff(BuffID.MagicPower))
+            {
+                player.statMana += 50;
+                player.statMana = Math.Min(player.statMana, player.statManaMax2);
+                return false;
+            }
+            else return base.OnPickup(item, player);
+        }
+    }
+}
---
+++ MagicPowerPot/ModPlayers/MagicPowerTweaks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShinyRemix.MagicPowerPot.ModPlayers
+{
+    public class MagicPowerTweaks : ModPlayer
+    {
+        public override void GetHealMana(Item item, bool quickHeal, ref int healValue)
+        {
+            if(ShinyOptions.MagicPowerBoost && Player.HasBuff(BuffID.MagicPower))
+            {
+                healValue = (int)Math.Floor(healValue / 2f);
+            }
+        }
+        public override void PostUpdateBuffs()
+        {
+            if (ShinyOptions.MagicPowerBoost && Player.HasBuff(BuffID.MagicPower))
+            {
+                Player.GetDamage(DamageClass.Magic) += 0.1f;
+                Player.GetCritChance(DamageClass.Magic) += 5f;
+            }
+        }
+    }
+}
